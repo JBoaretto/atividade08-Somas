73de33b7ff6b93a96df7672106ed4e5e90ff4d99
João Pedro <joaopedroboaretto@gmail.com>
2025-12-02 17:05:30 -0300

    Sequential Search implementation

--- somas.c
+++ somas.c
@@ -1,7 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char sequencial(int k, int *A, int N);
+
 int main(){
+    // Leitura de dados
+    int N; 
+    if(scanf("%d", &N) != 1) return 1;
+
+    int *A = (int *)malloc(N * sizeof(int));
+    if(A == NULL) return 1;
+
+    for(int i = 0; i < N; i++){
+        scanf("%d", &A[i]);
+    }
+
+    // Consultas
+    int M;
+    if(scanf("%d", &M) != 1){
+        free(A);
+        return 1;
+    }
+
+    int *K = (int *)malloc(M * sizeof(int));
+    if(K == NULL){
+        free(A);
+        return 1;
+    }
+
+    for (int i = 0; i < M; i++) {
+        scanf("%d", &K[i]);
+    }
+
+    // Processamento e saída
+    for (int i = 0; i < M; i++)
+    {
+        printf("%c\n", sequencial(K[i], A, N));
+    }
+    
+    free(A);
+    free(K);
 
     return 0;
+}
+
+char sequencial(int k, int *A, int N){
+    for(int i = 0; i < (N - 1); i++){
+        for (int j = (i + 1); j < N; j++)
+        {
+            if(A[i] + A[j] == k){
+                return 'S';
+            }
+        }
+    }
+    return 'N';
 }
